0c9661311bc890ff4d8f2be0adce5b595e3108be
Diego Rios Vasquez <diegovasquezcolombia@gmail.com>
2023-11-14 17:26:29 -0500

    Implement Non-sequential Range and Alternative Tokens/Divisors in IFizzBuzz.cs

--- EasyChallenge/FizzBuzz.Interview/TwistedFizzBuzz.Library/Interfaces/IFizzBuzz.cs
+++ EasyChallenge/FizzBuzz.Interview/TwistedFizzBuzz.Library/Interfaces/IFizzBuzz.cs
@@ -4,16 +4,8 @@ namespace TwistedFizzBuzz.Library.Interfaces
 {
     public interface IFizzBuzz
     {
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="start"></param>
-        /// <param name="end"></param>
-        /// <returns></returns>
         List<string> GenerateFizzBuzz(int start, int end);
-        //DONE: Implement Non-sequestian Range FizzBuzz: Accept user input of a non-sequential set of numbers and returns their FizzBuzz output. For example: -5, 6, 300, 12, 15
         List<string> GenerateFizzBuzz(int[] numbers);
-        //DONE: Implement with alternative tokens and divisors: Accept user input for alternative tokens instead of "Fizz" and "Buzz" and alternative divisors instead of 3 and 5. For example, 7, 17, and 3 would use "Poem", "Writer", and "College". 119 would output "PoemWriter", 51 would output "WriterCollege", 21 would output "PoemCollege, and 357 would output "PoemWriterCollege"
         List<string> GenerateFizzBuzz(int start, int end, List<(int divisor, string token)> divisorTokenPairs);
         List<string> GenerateFizzBuzz(int[] numbers, List<(int divisor, string token)> divisorTokenPairs);
         List<string> GenerateFizzBuzz(int start, int end, APIToken token);
