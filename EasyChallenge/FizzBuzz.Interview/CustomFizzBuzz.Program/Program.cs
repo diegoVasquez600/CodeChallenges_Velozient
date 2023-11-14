ed8150e796985322e9c219ae79f7c83a0c0c2b7f
Diego Alejandro Rios Vasquez <diegovasquezcool@outlook.com>
2023-11-14 01:41:14 -0500

    Add CustomFizzBuzz program and project files

---
+++ EasyChallenge/FizzBuzz.Interview/CustomFizzBuzz.Program/Program.cs
@@ -0,0 +1,18 @@
+﻿using TwistedFizzBuzz.Library;
+
+Console.WriteLine("Custom FizzBuzz Output Program");
+int start = -20;
+int end = 127;
+List< (int, string)> divisorTokenPairs = new()
+{
+    (5, "Fizz"),
+    (9, "Buzz"),
+    (27, "Bar")
+};
+
+FizzBuzz fizzBuzz = new();
+List<string> output = fizzBuzz.GenerateFizzBuzz(start, end, divisorTokenPairs);
+foreach (var item in output)
+{
+    Console.WriteLine(item);
+}
